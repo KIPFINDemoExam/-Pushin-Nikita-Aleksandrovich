f43a4526d442828f82dc8d361913fb322f0f36c7
9WSI <9WSI@DESKTOP-M591BD9>
2020-06-23 15:59:38 +0300

    testCommit

---
+++ .vs/-Pushin-Nikita-Aleksandrovich/v15/testApp/testApp/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            testDTForDemEntities1 ctx = new testDTForDemEntities1();
+
+
+
+
+            
+
+            
+
+            Users user = new Users();
+            user.name = "erty";
+            user.secondName = "ertySec";
+
+            ctx.Users.Add(user);
+
+            ctx.SaveChanges();
+
+            var users = from user1 in ctx.Users
+                        select user1.name;
+
+            List<string> usersList = users.ToList();
+
+            //MessageBox.Show(usersList[0].secondName);
+
+            listBox1.DataSource = usersList;
+
+
+
+
+
+
+        }
+    }
+}
